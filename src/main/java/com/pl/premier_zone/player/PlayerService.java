de51b29e4bb19d087857ab46cd57643ec982c3a1
janhlavati <janhlavaty11@gmail.com>
2025-12-04 23:27:08 +0100

    Configured the constructor of Player Service class

--- src/main/java/com/pl/premier_zone/player/PlayerService.java
+++ src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -1,7 +1,16 @@
 package com.pl.premier_zone.player;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
 }
