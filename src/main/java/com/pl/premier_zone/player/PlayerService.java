45e0808d4a4132f186651dd65d21deadc009b02b
janhlavati <janhlavaty11@gmail.com>
2025-12-06 12:25:21 +0100

    Added the rest of functionalities in PlayerService class

--- src/main/java/com/pl/premier_zone/player/PlayerService.java
+++ src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -1,9 +1,11 @@
 package com.pl.premier_zone.player;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -40,5 +42,29 @@ public class PlayerService {
         return playerRepository.findAll().stream().filter(player -> team.equals(player.getTeam()) && position.equals(player.getPos())).collect(Collectors.toList());
     }
 
+    public Player addPlayer(Player player) {
+        playerRepository.save(player);
+        return player;
+    }
 
+    public Player updatePlayer(Player updatedPlayer) {
+        Optional<Player> existingPlayer = playerRepository.findByName(updatedPlayer.getName());
+
+        if(existingPlayer.isPresent()) {
+            Player playerToUpdate = existingPlayer.get();
+            playerToUpdate.setName(updatedPlayer.getName());
+            playerToUpdate.setTeam(updatedPlayer.getTeam());
+            playerToUpdate.setPos(updatedPlayer.getPos());
+            playerToUpdate.setNation(updatedPlayer.getNation());
+
+            playerRepository.save(playerToUpdate);
+            return playerToUpdate;
+        }
+        return null;
+    }
+
+    @Transactional
+    public void deletePlayer(String playerName) {
+        playerRepository.deleteByName(playerName);
+    }
 }
