affd4774bd183db84ca2420b59678afd845dc4f9
janhlavati <janhlavaty11@gmail.com>
2025-12-08 00:46:17 +0100

    Added the functionality to add player in Controller class.

--- src/main/java/com/pl/premier_zone/player/PlayerController.java
+++ src/main/java/com/pl/premier_zone/player/PlayerController.java
@@ -1,10 +1,9 @@
 package com.pl.premier_zone.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +37,12 @@ public class PlayerController {
                 return playerService.getPlayers();
             }
         }
+
+    @PostMapping
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player) {
+        Player createdPlayer = playerService.addPlayer(player);
+        return new ResponseEntity<>(createdPlayer, HttpStatus.CREATED);
+    }
 
 
 }
