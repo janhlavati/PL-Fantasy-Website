6d36ad7c6fb777af50309c902ccac6b471b4674d
janhlavati <janhlavaty11@gmail.com>
2025-12-14 11:30:24 +0100

    Added the Home component

---
+++ Frontend/src/components/Home/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
+import Loadert from 'react-loaders';
+import LogoPL from '../../assets/images/logo-pl.png';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = "Welcome to".split('');
+    const jobArray = "Premier Zone Fantasy".split('');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <>
+            <div className="container-home-page">
+                <div className="text-zone">
+                    <h1>
+                        <img src={LogoPL} alt="Premier Zone Fantasy" />
+                        <br />
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={nameArray} idx={12} />
+                        <br />
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={jobArray} idx={15} />
+                    </h1>
+                    <h2>Your Ultimate Fantasy Football Experience</h2>
+                    <Link to="/teams" className="flat-button">Get Started</Link>
+                </div>
+            </div>
+            <Loadert type="pacman" />
+        </>
+    )
+}
+
+export default Home;
