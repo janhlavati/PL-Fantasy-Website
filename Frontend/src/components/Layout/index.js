05ee9f39c365065a9378fdc49e7e7843d9fc20b3
janhlavati <janhlavaty11@gmail.com>
2025-12-14 15:39:32 +0100

    Added the Layout component

---
+++ Frontend/src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import Sidebar from "../Sidebar";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+    return(
+        <div className="App">
+            <Sidebar />
+            <div className="page">
+                <Outlet />
+            </div>
+        </div>
+    )
+}
+export default Layout;
