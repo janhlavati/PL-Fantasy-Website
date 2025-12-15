ba5ee0dec78e4ffefd6e97900a2221687907412f
janhlavati <janhlavaty11@gmail.com>
2025-12-15 11:04:52 +0100

    Added the Nation component

---
+++ Frontend/src/components/Nation/index.js
@@ -0,0 +1,71 @@
+const Nations = () => {
+    const[letterClass, setLetterClass] = useState('text-animate');
+    const[searchQuery, setSearchQuery] = useState('');
+    const[filteredNations, setFilteredNations] = useState([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }   , 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const filtered = nationsData.filter(nation =>
+            nation.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setFilteredNations(filtered);
+    }, [searchQuery]);
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+    }
+
+    const renderCountryFlags = (countries) => {
+        return (
+            <div className="images-container">
+                {countries.map((country, idx) => (
+                    <div key={idx} className="image-box">
+                        <ReactCountryFlag
+                            countryCode={country.code}
+                            svg
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                            }}
+                        />
+                        <div className="content">
+                            <p className="title">{country.name}</p>
+                            <Link className="btn" to={`/data?nation=${encodeURIComponent(country.search)}`}>
+                                View
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+    return (
+        <>
+            <div className="container nation-page">
+                <h1 className="page-title">
+                    <AnimatedLetters letterClass={letterClass} strArray={"Nations".split("")} idx={15} />
+                </h1>
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Search for countries"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                    />
+                </div>
+                <div>{renderCountryFlags(filteredNations)}</div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
+}
+
+
+export default Nations
