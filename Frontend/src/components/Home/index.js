49a4054aa0eb2c2533bb036d361752f3e4c4eb33
janhlavati <janhlavaty11@gmail.com>
2025-12-24 00:33:13 +0100

    Added more design functionalities

--- Frontend/src/components/Home/index.js
+++ Frontend/src/components/Home/index.js
@@ -16,11 +16,9 @@ const Home = () => {
     return (
         <>
             <div className="container-home-page">
-                <div className="text-zone">
-                    <h1 style={{textAlign: 'center', fontSize: '24px'}}>Your Ultimate Fantasy Football Experience</h1>
-                    <div style={{ display: 'flex', justifyContent: 'center', color: '#fff', marginTop: '20px', fontSize: '18px'  }}>
-                        <Link to="/teams" className="flat-button">Get Started</Link>
-                    </div>
+                <div className="text-zone" style={{textAlign: 'center'}}>
+                    <b><h1 style={{display: 'inline-block', borderRadius: '30px', marginBottom: '110px', padding: '10px', fontSize: '48px', backgroundColor: '#100035ff'}}>Premier League Fantasy</h1></b>
+                    <Link to="/teams" className="flat-button" style={{ display: 'flex', justifyContent: 'center', color: 'black', fontSize: '18px', font: 'caption', color: '#ffffffff'}}>Get Started</Link>
                 </div>
             </div>
             <Loadert type="pacman" />
