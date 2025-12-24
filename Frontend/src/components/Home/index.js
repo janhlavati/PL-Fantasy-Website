c2794fa148dc9ebfe5972e2e6a7fd94df2a47072
janhlavati <janhlavaty11@gmail.com>
2025-12-25 00:21:54 +0100

    Added header css configuration

--- Frontend/src/components/Home/index.js
+++ Frontend/src/components/Home/index.js
@@ -17,7 +17,7 @@ const Home = () => {
         <>
             <div className="container-home-page">
                 <div className="text-zone" style={{textAlign: 'center'}}>
-                    <b><h1 style={{display: 'inline-block', borderRadius: '30px', marginBottom: '110px', padding: '10px', fontSize: '48px', backgroundColor: '#100035ff'}}>Premier League Fantasy</h1></b>
+                    <b><h1 style={{ display: 'block', borderRadius: '0px', marginBottom: '110px', padding: '10px', fontSize: '48px', background: 'linear-gradient(135deg,  #01263bff, #62005aff)'}}>Premier League Fantasy</h1></b>
                     <Link to="/teams" className="flat-button" style={{ display: 'flex', justifyContent: 'center', color: 'black', fontSize: '18px', font: 'caption', color: '#ffffffff'}}>Get Started</Link>
                 </div>
             </div>
